5dd5f88b915b543a90b8eeab5336178790118b06
Skyler Garrison <skyler.garrison@moxiworks.com>
2022-06-09 14:11:36 -0700

    heroku setup first commit

---
+++ traits_data.py
@@ -0,0 +1,24 @@
+traits = [
+    'Butts',
+    'MoreButts'
+]
+low_pop_traits = [
+]
+champions = {
+    'Butts':[
+        'Ahri',
+        'Brand',
+        'Malzahar',
+        'Swain',
+        'Vex',
+        'Viktor',
+        'Ziggs'
+    ],
+    'MoreButts':[
+        'Ekko',
+        'Kha\'zix',
+        'Nocturne',
+        'Talon',
+        'Twitch'
+    ]
+}
--- ultimatest_bravest.py
+++ ultimatest_bravest.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 roller = ub.Ub()
 
 @app.route("/")
-def hello_world():
+def display_roll():
     roller.roll()
     return f'<p><strong>Synergies:</strong> {roller.roll_result}</p><p><strong>Royal:</strong> {roller.royal}</p><p><strong>Champions:</strong> {roller.legal_champs}</p>'
