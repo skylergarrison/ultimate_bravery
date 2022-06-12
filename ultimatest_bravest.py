4d32ebdb7bca2b443de087d345fe05eb521ca064
Skyler Garrison <skyler.garrison@moxiworks.com>
2022-06-12 10:12:40 -0700

    add image capabilities and gitignore

--- ultimatest_bravest.py
+++ ultimatest_bravest.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 import ub
+import os
+
+THUMBNAIL_PATH = os.path.join('static', 'champ_thumbs')
 
 app = Flask(__name__)
+app.config['THUMBNAIL_FOLDER'] = THUMBNAIL_PATH
 
 roller = ub.Ub()
 
 @app.route("/")
 def display_roll():
     roller.roll()
-    return render_template('roll_result.html', result=roller)
+    full_filename = os.path.join(app.config['THUMBNAIL_FOLDER'], 'aatrox.png')
+    return render_template('roll_result.html', result=roller, photo=full_filename)
