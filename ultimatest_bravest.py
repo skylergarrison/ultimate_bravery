80d3a09357961dee854800d4f996c93851df7652
Sky Garrison <skyler.garrison@gmail.com>
2024-03-21 16:12:09 -0700

    url bad

--- ultimatest_bravest.py
+++ ultimatest_bravest.py
@@ -10,14 +10,9 @@ def index():
     roller.roll()
     return render_template('roll_result.html', result=roller)
 
-@app.route('/set10', methods=['GET', 'POST'])
+@app.route('/set11', methods=['GET', 'POST'])
 def set10():
     roller.roll()
-    return render_template('roll_result.html', result=roller)
-
-@app.route('/set3', methods=['GET', 'POST'])
-def set3():
-    roller.roll(3)
     return render_template('roll_result.html', result=roller)
 
 if __name__ == '__main__':
